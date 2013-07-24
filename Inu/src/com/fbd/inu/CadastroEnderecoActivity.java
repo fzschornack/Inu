b2ebbc042559ff3fa5bd006bc60c487c9964ef94
lucas <lucas@lucas-PC>
2013-07-24 16:34:52 -0300

    hhuhuuh

--- Inu/src/com/fbd/inu/CadastroEnderecoActivity.java
+++ Inu/src/com/fbd/inu/CadastroEnderecoActivity.java
@@ -1,15 +1,32 @@
 package com.fbd.inu;
 
+import org.json.JSONException;
+
+import com.fbd.inu.controle.ConexaoServidor;
+import com.fbd.inu.modelo.Endereco;
+import com.fbd.inu.modelo.Palestra;
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.widget.EditText;
 
 public class CadastroEnderecoActivity extends Activity {
+	
+	EditText endereco;
+	EditText numero;
+	EditText complemento;
+	
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_cadastro_endereco);
+		
+		endereco = (EditText) findViewById(R.id.editTextEndereco);
+		numero = (EditText) findViewById(R.id.editTextNumero);
+		complemento = (EditText) findViewById(R.id.editTextComplemento);
 	}
 
 	@Override
@@ -17,6 +34,16 @@ public class CadastroEnderecoActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.cadastro_endereco, menu);
 		return true;
+	}
+	
+	public void cadastrar(View v) throws JSONException {
+		Endereco u = new Endereco();
+		u.setEndereco(endereco.getText().toString());
+		u.setNumero(numero.getText().toString());
+		u.setComplemento(complemento.getText().toString());
+		
+		new ConexaoServidor().execute("http://10.0.2.2/inu/cadastro_endereco.php?"+u.toString());
+		
 	}
 
 }
--- Inu/src/com/fbd/inu/modelo/Endereco.java
+++ Inu/src/com/fbd/inu/modelo/Endereco.java
@@ -1,13 +1,86 @@
 package com.fbd.inu.modelo;
 
+import org.json.JSONException;
+import org.json.JSONStringer;
+
 public class Endereco {
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	private long idendereco;
+	private String endereco;
+	private String numero;
+	private String complemento;
+	private String bairro;
+	private String cidade;
+	private String estado;
+	private String pais;
+	private String maps;
+	public long getIdendereco() {
+		return idendereco;
 	}
+	public void setIdendereco(long idendereco) {
+		this.idendereco = idendereco;
+	}
+	public String getEndereco() {
+		return endereco;
+	}
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+	public String getNumero() {
+		return numero;
+	}
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+	public String getComplemento() {
+		return complemento;
+	}
+	public void setComplemento(String complemento) {
+		this.complemento = complemento;
+	}
+	public String getBairro() {
+		return bairro;
+	}
+	public void setBairro(String bairro) {
+		this.bairro = bairro;
+	}
+	public String getCidade() {
+		return cidade;
+	}
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
+	}
+	public String getEstado() {
+		return estado;
+	}
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+	public String getPais() {
+		return pais;
+	}
+	public void setPais(String pais) {
+		this.pais = pais;
+	}
+	public String getMaps() {
+		return maps;
+	}
+	public void setMaps(String maps) {
+		this.maps = maps;
+	}
+	@Override
+	public String toString() {
+		return "idendereco=" + idendereco + "&endereco=" + endereco
+				+ "&numero=" + numero + "&complemento=" + complemento
+				+ "&bairro=" + bairro + "&cidade=" + cidade + "&estado="
+				+ estado + "&pais=" + pais + "&maps=" + maps + "";
+	}
+
+	
+	
+
+
+	
+	
 
 }
